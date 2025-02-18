884d6ef28cd4d0f9d3e28ef2c92fd944c0cb8f34
unknown <lizzyoteje@gmail.com>
2025-02-18 05:35:43 +0100

    feat: navbar for search and add meal

--- src/components/RecipeNavbar/RecipeNavbar.tsx
+++ src/components/RecipeNavbar/RecipeNavbar.tsx
@@ -9,13 +9,7 @@ const RecipeNavbar: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
    const dispatch = useDispatch<AppDispatch>();
 
-  const handleSearchSubmit = () => {
-    if (searchTerm.trim()) {
-      dispatch(fetchMeals(searchTerm));
-    } else {
-      dispatch(fetchMeals('')); // Fetch all meals when the search term is empty
-    }
-  };
+ 
 
   return (
     <nav className="navbar">
@@ -32,11 +26,16 @@ const RecipeNavbar: React.FC = () => {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className="search-button" onClick={handleSearchSubmit}>
+         {searchTerm ? (
+          <button className="clear-button">
+            X
+          </button> 
+        ) :
+        <button className="search-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.414-1.414l-3.85-3.85a1.007 1.007 0 0 0-.116-.1zM12 6.5A5.5 5.5 0 1 1 1 6.5a5.5 5.5 0 0 1 11 0z"/>
           </svg>
-        </button>
+        </button>}
       </div>
       <button>Add Meal</button>
     </nav>
